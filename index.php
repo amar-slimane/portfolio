a2a46379e7b98cab6ee8aab14046242e2254826a
Slimane-amar <https://github.com/amar-slimane/reservation-salles.git>
2020-04-20 08:49:12 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato|Sigmar+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lobster|Montserrat|Sniglet&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald|Roboto+Condensed&display=swap" rel="stylesheet">
+    <title>Amar</title>
+</head>
+
+<body>
+    <header>
+        <nav id="haut" class="navbar">
+            <ul class="navbar_ul">
+                <a href="#about">
+                    <li class="navbar_li1">Présentaton</li>
+                </a>
+                <a href="#project">
+                    <li class="navbar_li2">Projets</li>
+                </a>
+            </ul>
+        </nav>
+        <div class="social_media"></div>
+    </header>
+    <main>
+        <div class="homepage">
+            <h1 class="home_h1">Amar Slimane</h1>
+            <img class="photo" src="img/foto2moi.jpg">
+            <h2 class="home_h2"> Developpeur Web Full-stack </h2>
+        </div>
+        <section>
+            <div class="title_presentation">
+                <h1 id="about">Présentation</h1>
+            </div>
+            <div class="presentation">
+
+
+                <h1 class="h1_descr">Description rapide.</h1>
+                <div class="p_description">
+                    <p class="p_descr">
+                        Developpeur quasi-full stack, prêt a en découdre avec le monde pro afin de parfaire mon expérience,
+                        d'étancher ma soif de <s>pouvoir</s> connaissances.
+                        Sinon je suis fan de jeux vidéos, notamment de jeux de combat, qui m'ont permis de développer des compétences tel que : se fixer des objectifs et les atteindres, rester humble, accepter la défaite pour mieux avancer.
+                    </p>
+                    <div class="stats">
+                        <ul>
+                            <h4>Statistiques</h4>
+                            <li>Travail d'équipe : A</li>
+                            <li>Humour : S</li>
+                            <li>Facilité d'apprentissage : A</li>
+                            <li>Gôut du Challenge : A</li>
+                            <li>Resistance au sommeil : C</li>
+
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+            <div id="part_2">
+                <div class="div_b">
+                    <div class="comp_div">
+                        <div class="php_progress">
+                            <img class="img_comp" src="img/php.png">
+                            <div class="bar_comp">
+                                <span class="percent_comp_php_sql"></span>
+
+                            </div>
+                            <p> 70% </p>
+                        </div>
+                        <div class="css_progress">
+                            <img class="img_comp" src="img/css.png">
+                            <div class="bar_comp">
+                                <span class="percent_comp">
+
+                            </div>
+                            <p> 85% </p>
+                        </div>
+                        <div class="html_progress">
+                            <img class="img_comp" src="img/html.png">
+
+                            <div class="bar_comp">
+                                <span class="percent_comp">
+
+                            </div>
+                            <p> 85% </p>
+                        </div>
+                        <div class="sql_progress">
+                            <img class="img_comp" src="img/sql.png">
+
+                            <div class="bar_comp">
+                                <span class="percent_comp_php_sql">
+
+                            </div>
+                            <p> 70% </p>
+                        </div>
+                    </div>
+                </div>
+                <div id="part_3">
+                    <div class="title_presentation">
+                        <h1 id="project">Projets</h1>
+                    </div>
+
+                    <div class="div_c">
+                        <div class="div_project_1">
+                            <div class="img_box"><img class="project_img" src="img/fansite.png"></div>
+                            <h1 class="h1_descr">Fansite South Park</h1>
+                            <p class="p_project">Site fait lors d'une project pool a la plateforme, le projet portais sur un site de fan. Lien
+                                Ce site m'a permis d'approfondir mes connaissances en HTML et CSS, notamment en terme d'utilisation du display flex, des tableaux, des animations, ainsi que bon nombre d'autres
+                                compétences. Lien <a target="_blank" href="site-sample/fansite/fansite.html">ici</a></p>
+                            <div class="comp_obt_1">
+                                <h1 class="h1_descr">Compétences principales : </h1>
+                                <img class="img_comp" src="img/html.png">
+                                <img class="img_comp" src="img/css.png">
+                            </div>
+
+                        </div>
+                        <div class="div_project_2">
+                            <div class="img_box"> <img class="project_img2" src="img/island.png"> </div>
+                            <h1 class="h1_descr">Site Voyages</h1>
+                            <p class="p_project"> Site crée en l'occasion d'une project pool a la plateforme. Sur le theme des voyages et du tourisme. Lien <a target="_blank" href="site-sample/voyages/voyages.html">ici</a></p>
+                            <div class="comp_obt_1">
+                                <h1 class="h1_descr">Compétences principales : </h1>
+                                <img class="img_comp" src="img/html.png">
+                                <img class="img_comp" src="img/css.png">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <nav class="navbar">
+            <div class="ul_footer_div">
+                <ul class="navbar_ul_foot">
+                    <a href="#about">
+                        <li class="navbar_li1">Présentaton</li>
+                    </a>
+                    <a href="#project">
+                        <li class="navbar_li2">Projets</li>
+                    </a>
+                    <a href="#haut">
+                        <li class="navbar_li3">Haut de page</li>
+                    </a>
+                </ul>
+            </div>
+            <div class="social_media">
+                <p>Contact :</p>
+                <a target="_blank" href="https://www.linkedin.com/in/amar-slimane-52b9b9199/"><img class="logo_img" src="img/linkedin.png"></a>
+
+            </div>
+        </nav>
+    </footer>
+</body>
+
+</html>
